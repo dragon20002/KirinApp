e0d641df010e070e0ee59965759c52ae93cf9aa0
dragon20002 <dragon20002@naver.com>
2022-01-09 00:12:03 +0900

    initial commit 2

---
+++ KirinApp-Server/src/test/kotlin/com/minuminu/haruu/kirinapp/KirinappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.minuminu.haruu.kirinapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KirinappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
